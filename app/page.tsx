bf54cd242f242f4f2180ac0c4e6ac98fda891c75
Hope-Harbor <hopeharbor12@gmail.com>
2025-03-23 18:13:21 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import RainbowText from "@/components/RainbowText";
 import RoleButtons from "@/components/RoleButtons";
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -9,6 +10,24 @@ export default function Home() {
       <header>
         <Link href="/" className="logo">InnerQuest</Link>
       </header>
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-32ZXDHP4RC"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-32ZXDHP4RC');
+          `,
+        }}
+      />
 
       {/* Main Content */}
       <main>

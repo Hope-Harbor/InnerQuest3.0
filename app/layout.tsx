897a78009b397934a46d6e17876e09ccf2dec311
Hope-Harbor <hopeharbor12@gmail.com>
2025-03-23 18:13:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css"; // Import global styles
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,6 +13,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           sizes="any"
         />
       </head>
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-32ZXDHP4RC"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-32ZXDHP4RC');
+          `,
+        }}
+      />
       <body>
         {children} {/* This renders the current page (e.g., Home, Questionnaire) */}
       </body>
